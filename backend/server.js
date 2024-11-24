d1697cd0009838f083c0ef26851429c8647628ee
SANDESHSOBARAD <sobaradsandesh@gmail.com>
2024-11-23 22:40:18 -0800

    backend controllers, model and routes implemented

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express'
-
-const PORT = 3000
-
+import dotenv from 'dotenv'
+import path from 'path';
+import { connectDB, PORT } from './config/db.js';
+import productsRoute from './routes/productsRoute.js'
+dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.listen(3000, () => {
+app.use("/api/products", productsRoute);
+
+app.listen(PORT, () => {
+    connectDB();
     console.log(`server started at port: ${PORT}, hello there`)
 })
